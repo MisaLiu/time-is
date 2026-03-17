077b1c0379a00e8ecbf584c462231a37310bb77f
Misa Liu <misaliu@misaliu.top>
2026-03-17 21:32:07 +0800

    feat(backend): Fetch time info from NTP server

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,15 +1,86 @@
+import 'dotenv/config';
 import express from 'express';
 import { createServer } from 'http';
+import NTPClient, { NTP_EVENTS } from 'ntp.js';
+
+const ntpMaps = new Map<number, NTPClient>();
 
 const app = express();
 
-app.get('/hello', (_, res) => {
-  res.send('Hello world!');
+app.get('/api/healthcheck', (_, res) => {
+  res
+    .type('json')
+    .send({
+      msg: 'ok'
+    });
+});
+
+app.get('/api/time', (req, res) => {
+  const reqTime = parseInt(req.query.reqTime as string);
+  if (!reqTime || isNaN(reqTime) || reqTime < 0) {
+    return res
+      .status(400)
+      .type('json')
+      .send({
+        msg: 'No reqTime provided'
+      });
+  }
+
+  const identity = Math.floor(Date.now() / 1000);
+  if (ntpMaps.has(identity)) {
+    return res
+      .status(409)
+      .type('json')
+      .send({
+        msg: 'Already had a client'
+      });
+  }
+
+  let ntpServer = process.env.NTP_SERVER || 'time.cloudflare.com';
+  if (req.query.ntpServer && typeof req.query.ntpServer === 'string') {
+    ntpServer = req.query.ntpServer;
+  }
+
+  const ntpClient = new NTPClient({
+    poolServerName: ntpServer,
+  });
+  ntpMaps.set(identity, ntpClient);
+
+  const clearNTPClient = () => {
+    ntpClient.stop();
+    ntpMaps.delete(identity);
+  }
+
+  ntpClient.on(NTP_EVENTS.SYNC, (time) => {
+    clearNTPClient();
+    res
+      .type('json')
+      .send({
+        msg: 'ok',
+        data: {
+          clientReqTime: reqTime,
+          ntpTime: (new Date(time)).getTime(),
+        }
+      });
+  });
+
+  ntpClient.on(NTP_EVENTS.ERROR, (error) => {
+    clearNTPClient();
+    res
+      .status(500)
+      .type('json')
+      .send({
+        msg: 'Failed to fetch NTP server',
+        error: error.toString(),
+      });
+  });
+
+  ntpClient.forceUpdate().then().catch();
 });
 
 const server = createServer(app);
-server.listen(3000);
-console.log('Server is listening on port 3000');
+server.listen(process.env.SERVER_PORT || 5000);
+console.log(`Server is listening on port ${process.env.SERVER_PORT || 5000}`);
 
 // For Vite's HMR compatibility
 if (import.meta.hot) {
